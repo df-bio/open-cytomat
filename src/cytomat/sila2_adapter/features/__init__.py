e09ea63c0ab12c048edf72e3ae581c27bae03a9b
Martin Patz <martin@patz.family>
2026-08-08 01:06:13 +0200

    feat(sila): add maintenance-controller feature stub

--- src/cytomat/sila2_adapter/features/__init__.py
+++ src/cytomat/sila2_adapter/features/__init__.py
@@ -2,6 +2,10 @@ from cytomat.sila2_adapter.features.barcode_scanner import (
     BARCODE_SCANNER_FEATURE,
     BarcodeScannerFeature,
 )
+from cytomat.sila2_adapter.features.maintenance_controller import (
+    MAINTENANCE_CONTROLLER_FEATURE,
+    MaintenanceControllerFeature,
+)
 from cytomat.sila2_adapter.features.plate_handler import (
     PLATE_HANDLER_FEATURE,
     PlateHandlerFeature,
@@ -10,6 +14,7 @@ from cytomat.sila2_adapter.features.plate_handler import (
 FEATURE_BINDINGS = [
     (PLATE_HANDLER_FEATURE, PlateHandlerFeature),
     (BARCODE_SCANNER_FEATURE, BarcodeScannerFeature),
+    (MAINTENANCE_CONTROLLER_FEATURE, MaintenanceControllerFeature),
 ]
 
 __all__ = ["FEATURE_BINDINGS"]
---
+++ src/cytomat/sila2_adapter/features/maintenance_controller/__init__.py
@@ -0,0 +1,3 @@
+from .maintenance_controller import MAINTENANCE_CONTROLLER_FEATURE, MaintenanceControllerFeature
+
+__all__ = ["MAINTENANCE_CONTROLLER_FEATURE", "MaintenanceControllerFeature"]
