414475225e528a38612c576226e9c5d26e803401
Martin Patz <martin@patz.family>
2026-08-12 23:34:34 +0200

    fix(sila): bind only implemented features

--- src/cytomat/sila2_adapter/features/__init__.py
+++ src/cytomat/sila2_adapter/features/__init__.py
@@ -1,30 +1,7 @@
-from cytomat.sila2_adapter.features.barcode_scanner import (
-    BARCODE_SCANNER_FEATURE,
-    BarcodeScannerFeature,
-)
-from cytomat.sila2_adapter.features.climate_controller import (
-    CLIMATE_CONTROLLER_FEATURE,
-    ClimateControllerFeature,
-)
-from cytomat.sila2_adapter.features.maintenance_controller import (
-    MAINTENANCE_CONTROLLER_FEATURE,
-    MaintenanceControllerFeature,
-)
-from cytomat.sila2_adapter.features.plate_handler import (
-    PLATE_HANDLER_FEATURE,
-    PlateHandlerFeature,
-)
-from cytomat.sila2_adapter.features.shaker_controller import (
-    SHAKER_CONTROLLER_FEATURE,
-    ShakerControllerFeature,
-)
+from cytomat.sila2_adapter.features.plate_handler import PLATE_HANDLER_FEATURE, PlateHandlerFeature
 
 FEATURE_BINDINGS = [
     (PLATE_HANDLER_FEATURE, PlateHandlerFeature),
-    (BARCODE_SCANNER_FEATURE, BarcodeScannerFeature),
-    (MAINTENANCE_CONTROLLER_FEATURE, MaintenanceControllerFeature),
-    (CLIMATE_CONTROLLER_FEATURE, ClimateControllerFeature),
-    (SHAKER_CONTROLLER_FEATURE, ShakerControllerFeature),
 ]
 
 __all__ = ["FEATURE_BINDINGS"]
